24186f11d19bde1d1b7afd1cfcc6a1c89c10425d
jia-yoon <60315608+jia-yoon@users.noreply.github.com>
2020-02-16 15:29:28 +0900

    so far

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+     hello? jia ?????
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
