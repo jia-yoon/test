5e3e808d6525c2fef3852986d4beb2d9049b599f
jia yoon <youandjia@gmail.com>
2020-02-28 16:57:23 +0900

    tabs_final

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,22 +5,23 @@
           <span class="headline">JARI</span>
           <span class="font-weight-light overline grey--text">  for OTSK</span>
           </v-toolbar-title>
-      <v-tabs v-model="tab" background-color="transparent" color="pink accent-3" centered>
-      <v-tab v-for="item in items" :key="item.tab" :to="item.to">
-        {{ item.tab }}
-      </v-tab>
-    </v-tabs>
+          <v-spacer></v-spacer>
       <v-toolbar-items>
-      <!-- <v-spacer></v-spacer> -->
       <v-btn icon>
       <v-icon>mdi-account-search</v-icon>
       </v-btn>
       <v-card-actions>
-        <v-spacer></v-spacer>
        <v-btn color="pink accent-3" text @click="signInWithGoogle">
            Sign In   </v-btn>
       </v-card-actions>
       </v-toolbar-items>
+      <template v-slot:extension>
+      <v-tabs v-model="tab" background-color="transparent" color="pink accent-3" grow>
+      <v-tab v-for="item in items" :key="item.tab" :to="item.to">
+        {{ item.tab }}
+      </v-tab>
+    </v-tabs>
+      </template>
     </v-app-bar></nav>
 </template>
 
